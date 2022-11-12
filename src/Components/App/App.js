2decb0559eea54a39015914f4faf976b3e9904e7
Emily Miles <ms.emily.j.miles@gmail.com>
2022-11-12 12:04:43 -0500

    Add useEffect, initial functionality, and styling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,13 +2,23 @@ import { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router';
 import './App.css';
 import Dashboard from '../Dashboard/Dashboard';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import { fetchUsers } from '../../apiCalls';
 
 const App = () => {
-  const [user, setUser] = useState();
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers()
+      .then(data => setUsers(data.users));
+  }, []);
 
   return (
     <main className='app-container'>
-
+      <Header users={ users } />
+      <Dashboard users={ users } />
+      <Footer />
     </main>
   );
 };
