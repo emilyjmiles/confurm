00c3180b53e837d3c916a7db6f5eee014f72774d
Emily Miles <ms.emily.j.miles@gmail.com>
2022-11-13 12:54:55 -0500

    Refactor to match data changes

--- src/Components/PetContainer/PetContainer.js
+++ src/Components/PetContainer/PetContainer.js
@@ -1,23 +1,18 @@
-import { useState, useEffect } from 'react';
 import './PetContainer.css';
 import PetCard from '../PetCard/PetCard';
-import { fetchUsers } from '../../apiCalls';
 
-const PetContainer = ({ users }) => {
-  const petCards = users.reduce((pets, user) => {
-    user.pets.forEach(pet => {
-      pets.push(
-        <PetCard
-          key={ pet.id + pet.name }
-          id={ pet.id }
-          image={ pet.image }
-          name={ pet.name }
-        />
-      );
-    });
+const PetContainer = ({ patients }) => {
+  const petCards = patients.map(patient => {
+    return (
+      <PetCard
+        key={ patient.id }
+        id={ patient.id }
+        image={ patient.image }
+        name={ patient.name }
+      />
+    );
+  });
 
-    return pets;
-  }, []);
 
   return (
     <section className='all-pets'>
