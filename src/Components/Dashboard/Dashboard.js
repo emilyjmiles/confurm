aa558288453b19e702a7e063be94be58dc463082
Emily Miles <ms.emily.j.miles@gmail.com>
2022-11-13 17:17:10 -0500

    Add method to match users to patients

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -3,11 +3,23 @@ import PetContainer from '../PetContainer/PetContainer';
 import Reminders from '../Reminders/Reminders';
 
 
-const Dashboard = ({ patients }) => {
+const Dashboard = ({ users, patients }) => {
+  const matchPatients = users.reduce((userPets, user) => {
+    user.pets.forEach(pet => {
+      patients.forEach(patient => {
+        if (pet.petId === patient.id) {
+          userPets.push(patient);
+        };
+      });
+    });
+
+    return userPets;
+  }, []);
+
   return (
     <section className='dashboard'>
-      <Reminders patients={ patients } />
-      <PetContainer patients={ patients } />
+      <Reminders patients={ matchPatients } />
+      <PetContainer patients={ matchPatients } />
     </section>
   );
 };
